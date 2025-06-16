256b4f1805e6c676b72d33ec0c085d04b06b0e43
BOUNEHMEElie <e.bounehme@equadis.com>
2025-06-17 00:10:31 +0300

    Finished it....

--- src/app/dashboard/pages/kennel/kennel.component.ts
+++ src/app/dashboard/pages/kennel/kennel.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 import {AppInputComponent} from "../../../shared-components/app-input/app-input.component";
 import {
   DogOverviewInformationStatusComponent
 } from '../../components/dog-overview-information-cards/dog-overview-information-status/dog-overview-information-status.component';
+import {scheduler} from 'dhtmlx-scheduler';
 
 @Component({
   selector: 'app-kennel',
@@ -11,8 +12,15 @@ import {
     DogOverviewInformationStatusComponent
   ],
   templateUrl: './kennel.component.html',
-  styleUrl: './kennel.component.scss'
+  styleUrl: './kennel.component.scss',
+  encapsulation: ViewEncapsulation.None,
+
 })
-export class KennelComponent {
+export class KennelComponent implements OnInit {
+  @ViewChild("scheduler_here", { static: true }) schedulerContainer!: ElementRef;
   items = Array.from({ length: 42 }, (_, i) => `Item ${i + 1}`);
+
+  ngOnInit() {
+    scheduler.init(this.schedulerContainer.nativeElement, new Date(2024, 9, 1));
+  }
 }
